ab888bd192b2d806640e3c80ed2ed3851f7d17d4
mchomans <01168876@pw.edu.pl>
2023-05-18 15:14:31 +0200

    add activating obstacle for given time

--- twitchgo/twitchgo/obstacle_receiver.py
+++ twitchgo/twitchgo/obstacle_receiver.py
@@ -1,5 +1,7 @@
 import rclpy
 from rclpy.node import Node
+import time
+import datetime
 
 from std_msgs.msg import Bool
 
@@ -18,12 +20,21 @@ class MotorObstacleReceiver(Node):
 
     def listener_callback(self, msg: Bool):
         if msg.data:
-            self._obstacel_mover.move_all(50, 50)
+            self.activate_obstacle()
         else:
             self._obstacel_mover.stop()
+    
+    def activate_obstacle(self):
+        self._obstacel_mover.move_all(50, 50)
+        time.sleep(20)
+        #second way of waiting
+        # start = time.time()
+        # while time.time()-start < 20:
+        #     pass
+        self._obstacel_mover.stop()
 
 
-class MotorObstacleReceiver(Node):
+class ServoObstacleReceiver(Node):
 
     def __init__(self, obstacle_name: str):
         super().__init__(obstacle_name)
@@ -35,10 +46,22 @@ class MotorObstacleReceiver(Node):
             self._obstacel_mover.moveUp()
         else:
             self._obstacel_mover.moveDown()
+    
             
 
 def main(args=None):
-    pass
+    rclpy.init(args=args)
+
+    motor_receiver = MotorObstacleReceiver()
+
+    rclpy.spin(motor_receiver)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    motor_receiver.destroy_node()
+    rclpy.shutdown()
+
 
 
 if __name__ == '__main__':
