7218ea85d18c0d15da735e1f98327d9c05a00bbf
Dangield <dan.gield@gmail.com>
2023-05-19 15:00:43 +0200

    Small improvements for OpenDoorDay

--- twitchgo/twitchgo/obstacle_receiver.py
+++ twitchgo/twitchgo/obstacle_receiver.py
@@ -4,9 +4,7 @@ import time
 import datetime
 
 from std_msgs.msg import Bool
-
-class Movement:
-    pass
+from move_all import Movement
 
 class Servomotor:
     pass
@@ -15,37 +13,40 @@ class MotorObstacleReceiver(Node):
 
     def __init__(self, obstacle_name: str):
         super().__init__(obstacle_name)
-        self._obstacel_mover = Movement()
+        self._obstacle_mover = Movement()
+        self._obstacle_mover.setup()
+        self.left = 1
+        self.right = 1
         self.subscription = self.create_subscription(Bool, f'{obstacle_name}_topic', self.listener_callback, 10)
 
     def listener_callback(self, msg: Bool):
         if msg.data:
             self.activate_obstacle()
         else:
-            self._obstacel_mover.stop()
+            self._obstacle_mover.stop()
     
-    def activate_obstacle(self):
-        self._obstacel_mover.move_all(50, 50)
-        time.sleep(20)
+    def activate_obstacle(self, left = True, right = True):
+        self._obstacle_mover.move_all(self.left * 50, self.right * 50)
+        time.sleep(30)
         #second way of waiting
         # start = time.time()
         # while time.time()-start < 20:
         #     pass
-        self._obstacel_mover.stop()
+        self._obstacle_mover.stop()
 
 
 class ServoObstacleReceiver(Node):
 
     def __init__(self, obstacle_name: str):
         super().__init__(obstacle_name)
-        self._obstacel_mover = Servomotor()
+        self._obstacle_mover = Servomotor()
         self.subscription = self.create_subscription(Bool, f'{obstacle_name}_topic', self.listener_callback, 10)
 
     def listener_callback(self, msg: Bool):
         if msg.data:
-            self._obstacel_mover.moveUp()
+            self._obstacle_mover.moveUp()
         else:
-            self._obstacel_mover.moveDown()
+            self._obstacle_mover.moveDown()
     
             
 
